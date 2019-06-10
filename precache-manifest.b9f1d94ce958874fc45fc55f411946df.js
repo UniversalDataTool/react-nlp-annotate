6d4ac507190fb4b0fc9d86aea4e6b18a08964033
seveibar <seveibar@gmail.com>
2019-06-10 00:57:08 -0400

    Updates

---
+++ precache-manifest.b9f1d94ce958874fc45fc55f411946df.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "35041e7f70b2dbaeb25d",
+    "url": "/react-nlp-annotate/static/css/main.a63b8a44.chunk.css"
+  },
+  {
+    "revision": "35041e7f70b2dbaeb25d",
+    "url": "/react-nlp-annotate/static/js/main.35041e7f.chunk.js"
+  },
+  {
+    "revision": "7513e375f71569d6d0d2",
+    "url": "/react-nlp-annotate/static/css/1.e5e8d078.chunk.css"
+  },
+  {
+    "revision": "7513e375f71569d6d0d2",
+    "url": "/react-nlp-annotate/static/js/1.7513e375.chunk.js"
+  },
+  {
+    "revision": "1185c96ae887ddb79781",
+    "url": "/react-nlp-annotate/static/js/runtime~main.1185c96a.js"
+  },
+  {
+    "revision": "891dcfc05485cf5459a42fa9766ce0cd",
+    "url": "/react-nlp-annotate/index.html"
+  }
+];
