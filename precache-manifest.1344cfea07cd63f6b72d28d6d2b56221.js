b70063e0ad5eba9459b56e1693cd7d4f7fc386da
Severin Ibarluzea <seveibar@gmail.com>
2019-07-21 17:17:07 -0400

    Updates

---
+++ precache-manifest.1344cfea07cd63f6b72d28d6d2b56221.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "11a9aef78fc707af045f",
+    "url": "/react-nlp-annotate/static/css/main.364dbca7.chunk.css"
+  },
+  {
+    "revision": "11a9aef78fc707af045f",
+    "url": "/react-nlp-annotate/static/js/main.11a9aef7.chunk.js"
+  },
+  {
+    "revision": "034f3e211e59a79c1b14",
+    "url": "/react-nlp-annotate/static/css/1.0d2bd1c1.chunk.css"
+  },
+  {
+    "revision": "034f3e211e59a79c1b14",
+    "url": "/react-nlp-annotate/static/js/1.034f3e21.chunk.js"
+  },
+  {
+    "revision": "1185c96ae887ddb79781",
+    "url": "/react-nlp-annotate/static/js/runtime~main.1185c96a.js"
+  },
+  {
+    "revision": "f928c3df7a51c8d50c1393bccfc31068",
+    "url": "/react-nlp-annotate/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/react-nlp-annotate/precache-manifest.e971986b9ae11857954d219fd1065c31.js"
+  "/react-nlp-annotate/precache-manifest.1344cfea07cd63f6b72d28d6d2b56221.js"
 );
 
 workbox.clientsClaim();
